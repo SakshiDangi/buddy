16b368f3e649e984c699cf76ea6bf64328f071cd
sakshi <panchalsakshi01@gmail.com>
2024-09-15 05:08:09 -0700

    health application dashbord client and server side

---
+++ src/lib/index.ts
@@ -0,0 +1,20 @@
+import { cn } from "./cn";
+import { db } from "./db";
+
+import { StepOneSchema, type StepOneSchemaType } from "./validators/step-one";
+import { StepTwoSchema, type StepTwoSchemaType } from "./validators/step-two";
+import { StepThreeSchema, type StepThreeSchemaType } from "./validators/step-three";
+import { StepFourSchema, type StepFourSchemaType } from "./validators/step-four";
+
+export {
+    cn,
+    db,
+    StepOneSchema,
+    StepOneSchemaType,
+    StepTwoSchema,
+    StepTwoSchemaType,
+    StepThreeSchema,
+    StepThreeSchemaType,
+    StepFourSchema,
+    StepFourSchemaType,
+}
